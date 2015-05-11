7a9a7bbc4223573f9b9bb2cf4aefa0faf3aaf654
Nabil J. Marquez <nabiljesus@gmail.com>
2015-05-11 01:26:46 -0430

    final

--- business/access_control/encrypt.py
+++ business/access_control/encrypt.py
@@ -1,5 +1,4 @@
 '''
-<<<<<<< HEAD:business/access_control/encrypt.py
 Created on 24/9/2014
 @author: Jean Carlos
 Modified by: Nabil Marquez and Meggie Sanchez
@@ -78,42 +77,3 @@ if __name__ == "__main__":
             print('Ha introducido el password correcto')
         else:
             print('El password es diferente')
-=======
-Created on May 6, 2015
-@author: Roberto y Nabil
-'''
-
-import user, encrypt
-from werkzeug import check_password_hash
-
-class clsLogin():
-    
-    def encriptar(self, value):
-        encri=encrypt.clsAccessControl()
-        return encri.encript(value)
-
-    def longitud(self, user_password):
-        longi=encrypt.clsAccessControl()
-        return longi.length_password(user_password)
-    
-    def check_password(self, username, trypass):
-        decri=encrypt.clsAccessControl()
-        usr=user.clsDBUser()
-        passw=usr.buscar(username).split() #Obteniendo el hash de la db
-        if passw!=[]:
-            print(passw)
-            passw=passw[1]
-            return decri.check_password(passw,trypass)
-        else:
-            return False
-def main():
-    logi=clsLogin()
-    print(logi.encriptar("hola"))
-    print(logi.encriptar("holaVAL3kas!?"))
-    print(logi.longitud("hola"))
-    print(logi.check_password("juanjuan2","baked"))
-    print(logi.check_password("juanjuan2","!uAn123456789101"))
-if __name__ == "__main__":
-   # stuff only to run when not called via 'import' here
-   main()
->>>>>>> dbe27b3975624db1a4a13595c94aafc763100c87:business/access-control/login.py
